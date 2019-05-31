f0607be40851daf43dcc711095ca7f9617f0dddc
Rishabh Bhandari <rishabh.bhandari53@gmail.com>
2019-05-31 15:34:55 +0530

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Rishabh Bhandari on 28/05/19.
+//  Copyright © 2019 Rishabh Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(" asaa ")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
